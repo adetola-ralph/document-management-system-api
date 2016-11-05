7123f0420bc0b74fd735fb3cbd12d87c8955bf53
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-05 16:09:28 +0100

    Replace the config.json file with config.js file

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'development': {
+    'username': 'postgres_user',
+    'password': 'MsniCUO12*',
+    'database': 'document_db',
+    'host': 'localhost',
+    'port': '5432',
+    'dialect': 'postgres'
+  },
+  'test': {
+    'username': 'update me',
+    'password': 'update me',
+    'database': 'update_me',
+    'host': 'update me',
+    'dialect': 'update me'
+  },
+  'production': {
+    'username': 'update me',
+    'password': 'update me',
+    'database': 'update me',
+    'host': 'update me',
+    'dialect': 'update me'
+  }
+};
