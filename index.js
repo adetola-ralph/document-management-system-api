c122d6e2a45c90212a2197da2359fdf2bfc5066e
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-08 23:02:37 +0100

    Add the ability for the port to be set by environment variables

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const express    = require('express');
 const app        = express();
 const bodyParser = require('body-parser');
 const morgan     = require('morgan');
-const port       = 8080;
-
+const port       = process.env.PORT || 8080;
 const router     = express.Router();
 
 const routes     = require('./routes');
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
