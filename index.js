738857fe5441deb26cb868f4fb785fc8a9f8ba6b
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-08 21:37:26 +0100

    Add routes to the index file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const morgan     = require('morgan');
 const port       = 8080;
 
+const router     = express.Router();
+
+const routes     = require('./routes');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -13,9 +17,10 @@ app.use(bodyParser.json());
 // Use morgan to diplay requests from the client
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json({response: 'root directory'});
-});
+// Implement the routes
+routes(router);
+
+app.use('/api', router);
 
 app.listen(port, function(){
   console.log(`Server started on port ${port}`);
