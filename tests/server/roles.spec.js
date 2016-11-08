912a19208787ee934477781f59b2691e42429362
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-08 22:19:30 +0100

    Add basic test to check if a unique role is created and all roles are returned

--- tests/server/roles.spec.js
+++ tests/server/roles.spec.js
@@ -8,12 +8,37 @@ const supertest = require('supertest');
 const server    = require('./../../index');
 const api       = supertest(server);
 
+const roleData  = require('./data/role-data');
+
 describe('Roles', () => {
   it('each role should have a unique title', (done) => {
-    done();
+    api
+      .post('/api/roles/')
+      .send(roleData.testRole)
+      .expect(201)
+      .end((err, res) => {
+        expect(res.message).to.equal(`${roleData.testRole.title} role created successfully`);
+        expect(res.data.title).to.equal(`${roleData.testRole.title}`);
+        done(err);
+      });
+
+      api
+        .post('/api/roles/')
+        .send(roleData.guestRole)
+        .expect(409)
+        .end((err, res) => {
+          expect(res.message).to.equal(`${roleData.guestRole.title} role exists`);
+          done(err);
+        });
   });
 
   it('should return all roles when Roles.all is called', (done) => {
-    done();
+    api
+      .get('/api/roles/')
+      .expect(200)
+      .end((err, res) => {
+        expect(res.data).to.equal(4);
+        done(err);
+      });
   });
 });
