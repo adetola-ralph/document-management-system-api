8a891b165e4b49b9cef902163513b78c7a68d388
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-11 15:30:26 +0100

    Import document route file to index route file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const role_routes = require('./roles');
-const user_routes = require('./users');
+const role_routes      = require('./roles');
+const user_routes      = require('./users');
+const document_routes = require('./documents');
 
 let routes = function(router) {
   role_routes(router);
   user_routes(router);
+  document_routes(router);
 };
 
 module.exports = routes;
