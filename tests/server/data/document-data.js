a08c136fe5da8c48934bb4b72be5c1ffbceb1963
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-11 21:00:57 +0100

    Add document data file for document tests

---
+++ tests/server/data/document-data.js
@@ -0,0 +1,27 @@
+module.exports = {
+  document1: {
+    title: 'document 1',
+    content: 'document 1 contents',
+    access: 'public'
+  },
+  document2: {
+    title: 'document 2',
+    content: 'document 2 contents',
+    access: 'private'
+  },
+  document3: {
+    title: 'document 3',
+    content: 'document 3 contents',
+    access: 'role'
+  },
+  document4: {
+    title: 'document 4',
+    content: 'document 4 contents',
+    access: 'private'
+  },
+  document5: {
+    title: 'document 5',
+    content: 'document 5 contents',
+    access: 'public'
+  }
+};
