71402634ecfd1e06f467fda824018cdf1489808d
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-12 11:51:24 +0100

    Add document seeder to add documents into the db before the document test

---
+++ seeders/docSeeder.js
@@ -0,0 +1,78 @@
+'use strict';
+const models = require('./../models/index');
+const doc = models.Documents;
+
+const docData = [
+  {
+    title: 'document 1',
+    content: 'document 1 contents',
+    access: 'private',
+    ownerId: 3
+  },
+  {
+    title: 'document 2',
+    content: 'document 2 contents',
+    access: 'role',
+    ownerId: 3
+  },
+  {
+    title: 'document 3',
+    content: 'document 3 contents',
+    access: 'private',
+    ownerId: 3
+  },
+  {
+    title: 'document 4',
+    content: 'document 4 contents',
+    access: 'public',
+    ownerId: 3
+  },
+  {
+    title: 'document 5',
+    content: 'document 5 contents',
+    access: 'private',
+    ownerId: 2
+  },
+  {
+    title: 'document 6',
+    content: 'document 6 contents',
+    access: 'role',
+    ownerId: 2
+  },
+  {
+    title: 'document 7',
+    content: 'document 7 contents',
+    access: 'private',
+    ownerId: 2
+  },
+  {
+    title: 'document 8',
+    content: 'document 8 contents',
+    access: 'public',
+    ownerId: 2
+  },
+  {
+    title: 'document 9',
+    content: 'document 9 contents',
+    access: 'public',
+    ownerId: 3
+  },
+  {
+    title: 'document 10',
+    content: 'document 10 contents',
+    access: 'public',
+    ownerId: 3
+  }
+];
+
+const docSeeder  = {
+  startSeed: () => {
+    doc.bulkCreate(docData).then(() => {
+      console.log('Document table seeded');
+    }).catch((err) => {
+      console.err(err);
+    });
+  }
+};
+
+module.exports = docSeeder;
--- tests/server/data/document-data.js
+++ tests/server/data/document-data.js
@@ -1,27 +1,12 @@
 module.exports = {
-  document1: {
-    title: 'document 1',
-    content: 'document 1 contents',
+  documentx: {
+    title: 'document x',
+    content: 'document x contents',
     access: 'public'
   },
-  document2: {
-    title: 'document 2',
-    content: 'document 2 contents',
-    access: 'private'
-  },
-  document3: {
-    title: 'document 3',
-    content: 'document 3 contents',
-    access: 'role'
-  },
-  document4: {
-    title: 'document 4',
-    content: 'document 4 contents',
-    access: 'private'
-  },
-  document5: {
-    title: 'document 5',
-    content: 'document 5 contents',
+  documenty: {
+    title: 'document y',
+    content: 'document y contents',
     access: 'public'
   }
 };
