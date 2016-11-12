322145769fb6e7edc4cb6ded3dc1d55cce2661db
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-11-12 07:51:38 +0100

    Implement the routing for documents

---
+++ routes/documents.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const docCtr        = require('./../controllers/documents.js');
+const authentication = require('./../middleware/authentication');
+const authorisation  = require('./../middleware/authorisation');
+
+const documentRoutes = (router) => {
+  router
+    .route('/documents')
+    .post(authentication, docCtr.create)
+    .get(authentication, docCtr.index);
+
+  router
+    .route('/documents/:id')
+    .get(authentication, docCtr.show)
+    .put(authentication, docCtr.update)
+    .delete(authentication, docCtr.delete);
+
+};
+
+module.exports = documentRoutes;
