1dd3dc0b2f33d5c3933d9f501594075783915d31
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2016-11-24 22:40:16 +0000

    Change how check for empt fields in authentication is implmenented

--- controllers/helpers/authHelper.js
+++ controllers/helpers/authHelper.js
@@ -1,15 +1,10 @@
 const authHelper = {
-  checkLoginDetails: (req, res) => {
-    let user = req.body;
+  checkLoginDetails: (req) => {
+    const user = req.body;
     if (!(user.username && user.password)) {
-      res.status(400)
-        .json({
-          success: false,
-          message: 'All fields must be filled'
-        });
-      res.end();
-      return true;
+      return false;
     }
+    return true;
   }
 };
 
